67b4916bbd97fe2b3bbf9c236b4c149240507943
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-22 19:29:54 -0400

    Creando el contexto

---
+++ P1-App1-JamesUrena/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using P1_App1_JamesUrena.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace P1_App1_JamesUrena.DAL
+{
+    public class Contexto : DbContext
+{ 
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Registros> Registros { get; set; }
+
+
+}
+}
+
+
+
+
+
