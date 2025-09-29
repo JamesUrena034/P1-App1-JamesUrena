89bb43bf611e32ac33d4d657c5dab790dff2e8e4
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-29 19:04:01 -0400

    Realizando mi clase Contexto

---
+++ P1-App1-JamesUrena/P1_App1_JamesUrena/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using P1_App1_JamesUrena.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace P1_App1_JamesUrena.DAL;
+
+ public class Contexto : DbContext
+{ 
+    public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+
+}
