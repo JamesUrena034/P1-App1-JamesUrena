0810c8ebd3b5e8c453f8a6a96289b62ec3b5d072
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-29 18:46:58 -0400

    Agregando Validaciones y Atributos a mi Modelo

--- P1-App1-JamesUrena/P1_App1_JamesUrena/Models/EntradasHuacales.cs
+++ P1-App1-JamesUrena/P1_App1_JamesUrena/Models/EntradasHuacales.cs
@@ -6,6 +6,18 @@ namespace P1_App1_JamesUrena.Models;
 public class EntradasHuacales
 {
     [Key]
-    public int RegistroId { get; set; }
+    public int EntradaId { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "El nombre del cliente es obligatorio")]
+    public string NombreCliente { get; set; } = string.Empty;
+
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0")]
+    public int Cantidad { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    public double Precio { get; set; }
 
 }
