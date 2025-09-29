14341b72b1520f222921a3c9d97aabb3cc97ae55
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-29 19:07:24 -0400

    inyectando Contexto

---
+++ P1-App1-JamesUrena/P1_App1_JamesUrena/Program.cs
@@ -0,0 +1,36 @@
+using P1_App1_JamesUrena.Components;
+using P1_App1_JamesUrena.DAL;
+using P1_App1_JamesUrena.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("SqlConnection");   
+builder.Services.AddDbContextFactory<Contexto>(options => options.UseSqlite(ConStr!));
+builder.Services.AddScoped<EntradasHuacalesService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
