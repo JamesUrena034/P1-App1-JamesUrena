d916a805a6c9b2d87577b96a8701661e8adc75ff
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-29 18:13:53 -0400

    Arreglando errores de mas de una solucion

---
+++ P1-App1-JamesUrena/P1_App1_JamesUrena/Service/RegistrosService.cs
@@ -0,0 +1,13 @@
+﻿using P1_App1_JamesUrena.Models;
+using P1_App1_JamesUrena.DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+
+
+namespace P1_App1_JamesUrena.Service;
+
+public class RegistrosService(IDbContextFactory<Contexto> DbFactory)
+{
+
+}
