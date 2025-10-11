4f657c76fd4f95060d3ead63e9b219909714a3dc
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-10-11 15:38:06 -0400

    Inyectando el Contexto

--- P1-App1-JamesUrena/P1_App1_JamesUrena/DAL/Contexto.cs
+++ P1-App1-JamesUrena/P1_App1_JamesUrena/DAL/Contexto.cs
@@ -6,9 +6,40 @@ using Microsoft.EntityFrameworkCore;
 namespace P1_App1_JamesUrena.DAL;
 
  public class Contexto : DbContext
-{ 
-    public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+{
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+    public DbSet<TiposHuacales> TiposHuacales { get; set; }
+    public DbSet<EntradasHuacalesDetalles> EntradasHuacalesDetalles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<TiposHuacales>().HasData(
+             new List<TiposHuacales>()
+             {
+            new()
+            {
+                TipoId = 1,
+                Descripcion = "Huacales verdes",
+                Existencia =0
+            },
+            new()
+            {
+                TipoId = 2,
+                Descripcion = "Huacales azules",
+                Existencia =0
+            },
+            new()
+            {
+                TipoId = 3,
+                Descripcion = "Huacales negros",
+                Existencia =0
+            },
+             }
+         );
+        base.OnModelCreating(modelBuilder);
+    }
 
 
 }
