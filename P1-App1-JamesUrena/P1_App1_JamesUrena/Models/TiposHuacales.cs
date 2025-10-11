8a764b3defb61ce031e56191af55eb104318dbf7
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-10-11 15:41:05 -0400

    Realizando Modelos y Modifcando

---
+++ P1-App1-JamesUrena/P1_App1_JamesUrena/Models/TiposHuacales.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_App1_JamesUrena.Models;
+
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId {  get; set; }
+
+    [Required(ErrorMessage ="Debe de tener una Descripcion Obligatoria")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue, ErrorMessage ="La Existencia debe ser Mayor o igual a 0")]
+    public int Existencia { get; set; }
+
+
+    [InverseProperty("TipoHuacales")]
+    public virtual ICollection<EntradasHuacalesDetalles> EntradasHuacalesDetalle { get; set; } = new List<EntradasHuacalesDetalles>();
+}
